c3d60f3f61965b8f9d656f44b249a0293c5b467a
Luana Dias <luana.dias@colab.re>
2025-02-24 20:44:00 -0300

    created: flush bar

--- lib/login/widgets/login_button.dart
+++ lib/login/widgets/login_button.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_clean/bloc/login_bloc.dart';
 import 'package:flutter_bloc_clean/utils/enums.dart';
+import 'package:flutter_bloc_clean/utils/flush_bar_helper.dart';
 
 class LoginButton extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
   final formKey;
 
   const LoginButton({super.key, required this.formKey});
@@ -11,31 +13,23 @@ class LoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocListener<LoginBloc, LoginStates>(
+      listenWhen: (current, previous) =>
+          current.postApiStatus != previous.postApiStatus,
       listener: (context, state) {
         if (state.postApiStatus == PostAPiStatus.error) {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text(state.message.toString())),
-            );
+          FlushBarHelper.flushBarErrorMessage(
+              state.message.toString(), context);
         }
         if (state.postApiStatus == PostAPiStatus.success) {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text(state.message.toString())),
-            );
+          FlushBarHelper.flushBarErrorMessage('login succesfull', context);
         }
         if (state.postApiStatus == PostAPiStatus.loading) {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              const SnackBar(content: Text('submitting')),
-            );
+          FlushBarHelper.flushBarErrorMessage('submitting', context);
         }
       },
       child: BlocBuilder<LoginBloc, LoginStates>(
-        buildWhen: (previous, current) => false,
+        buildWhen: (current, previous) =>
+            current.postApiStatus != previous.postApiStatus,
         builder: (context, state) {
           return ElevatedButton(
               onPressed: () {
@@ -43,7 +37,9 @@ class LoginButton extends StatelessWidget {
                   context.read<LoginBloc>().add(LoginApi());
                 }
               },
-              child: const Text('login'));
+              child: state.postApiStatus == PostAPiStatus.loading
+                  ? const CircularProgressIndicator()
+                  : const Text('login'));
         },
       ),
     );
---
+++ lib/utils/flush_bar_helper.dart
@@ -0,0 +1,16 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+
+class FlushBarHelper {
+  static void flushBarErrorMessage(String message, BuildContext context) {
+    showFlushbar(
+        context: context,
+        flushbar: Flushbar(
+          forwardAnimationCurve: Curves.decelerate,
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          padding: const EdgeInsets.all(15),
+          message: message,
+        )..show(context));
+  }
+}
